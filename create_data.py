61f492a6e8853b5d1efdf9445bae318c43f048a7
sachin <sachin[at]scoopwhoop[dot]com>
2016-06-10 16:42:51 +0530

    tabs to spaces

--- create_data.py
+++ create_data.py
@@ -9,7 +9,8 @@ import requests
 import os
 import unicodedata
 
-# categories array to be fetched from the ScoopWhoop url source page of each post
+# categories array to be fetched from the ScoopWhoop url source page of
+# each post
 categories = []
 
 # 2 D array containing array of keywords of each post
@@ -17,163 +18,169 @@ keywords = []
 
 
 def get_page_links(data):
-	' Returns a list of ScoopWhoop page urls to be used to get categories and keywords '
+    ' Returns a list of ScoopWhoop page urls to be used to get categories and keywords '
 
-	pages = []
+    pages = []
 
-	# Getting page url from each post
-	for d in data:
-		pages.append(d['link'])
+    # Getting page url from each post
+    for d in data:
+        pages.append(d['link'])
 
-	return pages
+    return pages
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
+
 
 def get_categories_keywords(pages):
-	' Fills global arrays categories[] and keywords[] from page URLs '
+    ' Fills global arrays categories[] and keywords[] from page URLs '
 
-	global categories, keywords
+    global categories, keywords
 
-	passes = 0
+    passes = 0
 
-	for page in pages:
-		# Temporary string of keywords which is to be stripped and splitted at commas to get a list of keywords
-		keys = ""
+    for page in pages:
+        # Temporary string of keywords which is to be stripped and splitted at
+        # commas to get a list of keywords
+        keys = ""
 
-		response = urllib2.urlopen(page)
+        response = urllib2.urlopen(page)
 
-		source = response.read()
+        source = response.read()
 
-		soup = BeautifulSoup(source, 'html.parser')
+        soup = BeautifulSoup(source, 'html.parser')
 
-		# Iterating to get categories array and temporary string of keywords
-		for meta in soup.find_all('meta'):
-			if(meta.get('property')=='category'):
-				categories.append(str(meta.get('content')))
+        # Iterating to get categories array and temporary string of keywords
+        for meta in soup.find_all('meta'):
+            if(meta.get('property') == 'category'):
+                categories.append(str(meta.get('content')))
 
-			if(meta.get('name')=='keywords'):
-				keys = str(meta.get('content'))
+            if(meta.get('name') == 'keywords'):
+                keys = str(meta.get('content'))
 
+        if len(categories) != passes + 1:
+            categories.append('others'.encode("utf-8"))
 
-		if len(categories) != passes + 1:
-			categories.append('others'.encode("utf-8"))
+        # Process temporary string by List Comprehension
+        tt = [x.strip() for x in keys.split(',')]
 
-		# Process temporary string by List Comprehension
-		tt = [x.strip() for x in keys.split(',')]
+        keywords.append(tt)
 
-		keywords.append(tt)
+        passes = passes + 1
 
-		passes = passes + 1
+        print categories[passes - 1]
 
-		print categories[passes-1]
-
-		print "Completed " + str(passes)
+        print "Completed " + str(passes)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
+
 
 def get_index_from_json(data, key, value):
-	' Returns an index reading JSON data to find the value at the particular key '
+    ' Returns an index reading JSON data to find the value at the particular key '
 
-	# Searching key-value pair in all indices
-	for idx in xrange( len(data['insights']['data']) ):
-		if data['insights']['data'][idx][key] == value:
-			return idx
-	
+    # Searching key-value pair in all indices
+    for idx in xrange(len(data['insights']['data'])):
+        if data['insights']['data'][idx][key] == value:
+            return idx
+
 
 def write_into_csv(data, owriter, owriter1):
-	' Writes into two csv files data.csv and keywords_data.csv parsing the JSON data file'
+    ' Writes into two csv files data.csv and keywords_data.csv parsing the JSON data file'
 
-        owriter.writerow(['id','name','message','category', 'likes', 'shares', 'comments', 'ctr'])
-	owriter1.writerow(['id', 'keywords', 'likes', 'shares', 'comments', 'ctr'])
+    owriter.writerow(['id', 'name', 'message', 'category',
+                      'likes', 'shares', 'comments', 'ctr'])
+    owriter1.writerow(['id', 'keywords', 'likes', 'shares', 'comments', 'ctr'])
 
-	# Key and Search Pattern to search for the index
-	key = 'name'
-	search_pattern = "post_stories_by_action_type"
+    # Key and Search Pattern to search for the index
+    key = 'name'
+    search_pattern = "post_stories_by_action_type"
 
-        category_idx=0
-	row = 0
+    category_idx = 0
+    row = 0
 
-	# Fill data.csv rows for each post
-        for d in data:
-		ctr = calc_ctr(d)
-	
-		# Use this index value to get likes, comments and shares of each post
-	        idx = get_index_from_json(d, key, search_pattern)
+    # Fill data.csv rows for each post
+    for d in data:
+        ctr = calc_ctr(d)
 
-		if 'name' in d.keys():
-                        name = d['name']
-			name = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
-                else:
-                        name = ''
+        # Use this index value to get likes, comments and shares of each post
+        idx = get_index_from_json(d, key, search_pattern)
 
-		if 'message' in d.keys():
-			message = d['message']
-			message = unicodedata.normalize('NFKD', message).encode('ascii','ignore')
-		else:
-			message = ''
+        if 'name' in d.keys():
+            name = d['name']
+            name = unicodedata.normalize(
+                'NFKD', name).encode('ascii', 'ignore')
+        else:
+            name = ''
 
-		if 'like' in d['insights']['data'][idx]['values'][0]['value'].keys():
-                        like = d['insights']['data'][idx]['values'][0]['value']['like']
-                else:
-                        like = 0
+        if 'message' in d.keys():
+            message = d['message']
+            message = unicodedata.normalize(
+                'NFKD', message).encode('ascii', 'ignore')
+        else:
+            message = ''
 
+        if 'like' in d['insights']['data'][idx]['values'][0]['value'].keys():
+            like = d['insights']['data'][idx]['values'][0]['value']['like']
+        else:
+            like = 0
 
-		if 'share' in d['insights']['data'][idx]['values'][0]['value'].keys():
-                        share = d['insights']['data'][idx]['values'][0]['value']['share']
-                else:
-                        share = 0
+        if 'share' in d['insights']['data'][idx]['values'][0]['value'].keys():
+            share = d['insights']['data'][idx]['values'][0]['value']['share']
+        else:
+            share = 0
 
-		if 'comment' in d['insights']['data'][idx]['values'][0]['value'].keys():
-                        comment = d['insights']['data'][idx]['values'][0]['value']['comment']
-                else:
-                        comment = 0
+        if 'comment' in d['insights']['data'][idx]['values'][0]['value'].keys():
+            comment = d['insights']['data'][idx][
+                'values'][0]['value']['comment']
+        else:
+            comment = 0
 
+        owriter.writerow([
+            d['id'],
+            name,
+            message,
+            categories[category_idx],
+            like,
+            share,
+            comment,
+            ctr])
 
-		owriter.writerow([
-				d['id'],
-				name,
-				message,
-				categories[category_idx],
-				like,
-				share, 
-				comment, 
-				ctr])
-		
-		for column in xrange(len(keywords[row])):
-                        owriter1.writerow([
-                                        d['id'],
-                                        keywords[row][column],
-					like,
-                                	share,   
-                                	comment, 
-                                	ctr])
+        for column in xrange(len(keywords[row])):
+            owriter1.writerow([
+                d['id'],
+                keywords[row][column],
+                like,
+                share,
+                comment,
+                ctr])
 
-		row = row + 1
-                category_idx = category_idx + 1
+        row = row + 1
+        category_idx = category_idx + 1
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
+
 def calc_ctr(d):
-	' Returns Click-Through Rate i.e. clicks per post reach rate '
+    ' Returns Click-Through Rate i.e. clicks per post reach rate '
 
-	ctr = 0
+    ctr = 0
 
-	# Getting index to know the count of post reaches
-	idx = get_index_from_json(d, 'name', 'post_impressions_unique')
-	
-	if d['insights']['data'][idx]['values'][0]['value']:
-		reach = d['insights']['data'][idx]['values'][0]['value']
-	else:
-		reach=0
+    # Getting index to know the count of post reaches
+    idx = get_index_from_json(d, 'name', 'post_impressions_unique')
 
-	sum_clicks = 0
+    if d['insights']['data'][idx]['values'][0]['value']:
+        reach = d['insights']['data'][idx]['values'][0]['value']
+    else:
+        reach = 0
 
-	# Getting index to know the count of different type of clicks
-	idx = get_index_from_json(d, 'name', 'post_consumptions_by_type')
+    sum_clicks = 0
 
-	for clicks in d['insights']['data'][idx]['values'][0]['value']:
-		sum_clicks = float(d['insights']['data'][idx]['values'][0]['value'][clicks]) + sum_clicks
-		'''other_clicks = d['insights']['data'][idx]['values'][0]['value']['other clicks']
+    # Getting index to know the count of different type of clicks
+    idx = get_index_from_json(d, 'name', 'post_consumptions_by_type')
+
+    for clicks in d['insights']['data'][idx]['values'][0]['value']:
+        sum_clicks = float(d['insights']['data'][idx]['values'][
+                           0]['value'][clicks]) + sum_clicks
+        '''other_clicks = d['insights']['data'][idx]['values'][0]['value']['other clicks']
 		link_clicks = d['insights']['data'][idx]['values'][0]['value']['link clicks']
 		photoviews = d['insights']['data'][idx]['values'][0]['value']
 		if 'photo view' in photoviews:
@@ -189,49 +196,48 @@ def calc_ctr(d):
 
 		click = float(other_clicks + link_clicks + photo_views)'''
 
-	if sum_clicks==0 and reach==0:
-		ctr =0
-	else:    
-		ctr = format((sum_clicks/reach)*100, '.2f')
+    if sum_clicks == 0 and reach == 0:
+        ctr = 0
+    else:
+        ctr = format((sum_clicks / reach) * 100, '.2f')
 
-	return ctr
+    return ctr
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
-if __name__== '__main__':
-	while True:
-		print 'loading JSON data. . .\n'
+if __name__ == '__main__':
+    while True:
+        print 'loading JSON data. . .\n'
 
-		r = requests.get('http://172.18.2.209:8090/')
-		'''with open('fb_posts.json') as f:
+        r = requests.get('http://172.18.2.209:8090/')
+        '''with open('fb_posts.json') as f:
 			data = json.load(f)'''
 
-		data = r.json()
+        data = r.json()
 
-		csv_file = open('fb_posts_data.csv','w')
-		csv_file1 = open('keywords_data.csv', 'w')
+        csv_file = open('fb_posts_data.csv', 'w')
+        csv_file1 = open('keywords_data.csv', 'w')
 
-		owriter = csv.writer(csv_file)
-		owriter1 = csv.writer(csv_file1)
+        owriter = csv.writer(csv_file)
+        owriter1 = csv.writer(csv_file1)
 
-		print 'get page links. . .\n'
+        print 'get page links. . .\n'
 
-		pages = get_page_links(data)
+        pages = get_page_links(data)
 
-		print 'loading categories and keywords. . .\n'
+        print 'loading categories and keywords. . .\n'
 
-		get_categories_keywords(pages)
+        get_categories_keywords(pages)
 
-		print 'writing into csv files. . .\n'
+        print 'writing into csv files. . .\n'
 
-		write_into_csv(data, owriter, owriter1)
+        write_into_csv(data, owriter, owriter1)
 
-		print 'data loaded successfully!\n'
+        print 'data loaded successfully!\n'
 
-		csv_file.close()
-		csv_file1.close()
+        csv_file.close()
+        csv_file1.close()
 
-		os.system('python prep_data.py')
+        os.system('python prep_data.py')
 
-		time.sleep(60)
-
+        time.sleep(7200)
--- prep_data.py
+++ prep_data.py
@@ -7,23 +7,26 @@ from sqlalchemy.orm import sessionmaker
 import csv
 import sys
 
-def load_data(file_name):
-	csv_data = []
-	with open(file_name, 'rb') as f:
-		data = csv.reader(f)
-		for d in data:
-			csv_data.append(d)
 
-	return csv_data
-	
+def load_data(file_name):
+    csv_data = []
+    with open(file_name, 'rb') as f:
+        data = csv.reader(f)
+        for d in data:
+            csv_data.append(d)
+
+    return csv_data
+
 Base = declarative_base()
 
+
 class FB_Data(Base):
-    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+    # Tell SQLAlchemy what the table name is and if there's any table-specific
+    # arguments it should know about
     __tablename__ = 'FB_POSTS_DATA'
-    
-    #tell SQLAlchemy the name of column and its attributes:
-    id = Column(String, primary_key=True, nullable=False) 
+
+    # tell SQLAlchemy the name of column and its attributes:
+    id = Column(String, primary_key=True, nullable=False)
     name = Column(String(100))
     message = Column(String(100))
     category = Column(String(100))
@@ -32,10 +35,11 @@ class FB_Data(Base):
     comment = Column(Integer)
     ctr = Column(Float)
 
+
 class Key_Data(Base):
     __tablename__ = 'KEYWORDS_DATA'
 
-    #tell SQLAlchemy the name of column and its attributes:
+    # tell SQLAlchemy the name of column and its attributes:
     id = Column(String(100))
     keywords = Column(String(100), primary_key=True)
     like = Column(Integer)
@@ -46,13 +50,13 @@ class Key_Data(Base):
 if __name__ == "__main__":
     t = time()
 
-    #Create the database
+    # Create the database
     engine = create_engine('sqlite:///myData.sqlite')
     #engine.raw_connection().connection.text_factory = str
 
     Base.metadata.create_all(engine)
 
-    #Create the session
+    # Create the session
     session = sessionmaker()
     session.configure(bind=engine)
     s = session()
@@ -60,33 +64,33 @@ if __name__ == "__main__":
     ids = []
 
     for dt in s.query(FB_Data):
-	ids.append(str(dt.id))
+        ids.append(str(dt.id))
 
     file_name1 = "fb_posts_data.csv"
     file_name2 = "keywords_data.csv"
-    
+
     fb_data = load_data(file_name1)
     key_data = load_data(file_name2)
 
     for i in range(len(fb_data)):
-	if i>0:
-		if fb_data[i][0] not in ids:
-			ids.append(fb_data[i][0])
-		else:
-			dat1 = s.query(FB_Data).filter_by(id=fb_data[i][0]).first()
-			s.delete(dat1)
+        if i > 0:
+            if fb_data[i][0] not in ids:
+                ids.append(fb_data[i][0])
+            else:
+                dat1 = s.query(FB_Data).filter_by(id=fb_data[i][0]).first()
+                s.delete(dat1)
 
-		fb_record = FB_Data(**{
-				'id' : fb_data[i][0],
-				'name' : fb_data[i][1],
-				'message' : fb_data[i][2],
-				'category' : fb_data[i][3],
-				'like' : fb_data[i][4],
-				'share' : fb_data[i][5],
-				'comment' : fb_data[i][6],
-				'ctr': fb_data[i][7]
-                         })
-		s.add(fb_record)
+            fb_record = FB_Data(**{
+                'id': fb_data[i][0],
+                'name': fb_data[i][1],
+                'message': fb_data[i][2],
+                'category': fb_data[i][3],
+                'like': fb_data[i][4],
+                'share': fb_data[i][5],
+                'comment': fb_data[i][6],
+                'ctr': fb_data[i][7]
+            })
+            s.add(fb_record)
 
     keys = []
 
@@ -94,27 +98,27 @@ if __name__ == "__main__":
         keys.append(str(dt.keywords))
 
     for i in range(len(key_data)):
-	if i>0:
-		if key_data[i][1] not in keys:
-			keys.append(key_data[i][1])
-		else:
-			dat2 = s.query(Key_Data).filter_by(keywords=key_data[i][1]).first()
-			s.delete(dat2)
+        if i > 0:
+            if key_data[i][1] not in keys:
+                keys.append(key_data[i][1])
+            else:
+                dat2 = s.query(Key_Data).filter_by(
+                    keywords=key_data[i][1]).first()
+                s.delete(dat2)
 
-		key_record = Key_Data(**{
-                                'id' : key_data[i][0],
-				'keywords': key_data[i][1],
-                                'like' : key_data[i][2],
-                                'share' : key_data[i][3],
-                                'comment' : key_data[i][4],
-                                'ctr': key_data[i][5]
-                         })
+            key_record = Key_Data(**{
+                'id': key_data[i][0],
+                'keywords': key_data[i][1],
+                'like': key_data[i][2],
+                'share': key_data[i][3],
+                'comment': key_data[i][4],
+                'ctr': key_data[i][5]
+            })
 
-		s.add(key_record)
+            s.add(key_record)
 
-    s.commit() #Attempt to commit all the records
-   
-    s.close() #Close the connection
-    
-    print "Time elapsed: " + str(time() - t) + " s." #0.091s
+    s.commit()  # Attempt to commit all the records
 
+    s.close()  # Close the connection
+
+    print "Time elapsed: " + str(time() - t) + " s."  # 0.091s
