9c0a4a3b11bf3636f88eab33aea9e76c4cf28f55
sachin <sachin[at]scoopwhoop[dot]com>
2016-06-17 13:21:38 +0530

    indented code

--- create_data.py
+++ create_data.py
@@ -10,336 +10,359 @@ import os
 import unicodedata
 import logging
 
-# categories array to be fetched from the ScoopWhoop url source page of each post
+# categories array to be fetched from the ScoopWhoop url source page of
+# each post
 categories = []
 
 # 2 D array containing array of keywords of each post
 keywords = []
 
+
 def get_page_links(data):
-	' Returns a list of ScoopWhoop page urls to be used to get categories and keywords '
+    ' Returns a list of ScoopWhoop page urls to be used to get categories and keywords '
 
-	pages = []
+    pages = []
 
-	# Getting page url from each post
-	for d in data:
-		pages.append(d['link'])
+    # Getting page url from each post
+    for d in data:
+        pages.append(d['link'])
 
-	return pages
+    return pages
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
+
 
 def get_data_from_post(pages):
-	' Fills global arrays categories[] and keywords[] from page URLs '
+    ' Fills global arrays categories[] and keywords[] from page URLs '
 
-	global categories, keywords, no_of_images, images
+    global categories, keywords, no_of_images, images
 
-	passes = 0
+    passes = 0
 
-	for page in pages:
-		# Temporary string of keywords which is to be stripped and splitted at commas to get a list of keywords
-		keys = ""
+    for page in pages:
+        # Temporary string of keywords which is to be stripped and splitted at
+        # commas to get a list of keywords
+        keys = ""
 
-		response = urllib2.urlopen(page)
+        response = urllib2.urlopen(page)
 
-		source = response.read()
+        source = response.read()
 
-		soup = BeautifulSoup(source, 'html.parser')
+        soup = BeautifulSoup(source, 'html.parser')
 
-		# Iterating to get categories array and temporary string of keywords
-		for meta in soup.find_all('meta'):
-			if(meta.get('property')=='category'):
-				categories.append(str(meta.get('content')))
+        # Iterating to get categories array and temporary string of keywords
+        for meta in soup.find_all('meta'):
+            if(meta.get('property') == 'category'):
+                categories.append(str(meta.get('content')))
 
-			if(meta.get('name')=='keywords'):
-				keys = str(meta.get('content'))
+            if(meta.get('name') == 'keywords'):
+                keys = str(meta.get('content'))
 
-		if len(categories) != passes + 1:
-			categories.append('others'.encode("utf-8"))
+        if len(categories) != passes + 1:
+            categories.append('others'.encode("utf-8"))
 
-		# Process temporary string by List Comprehension
-		tt = [x.strip() for x in keys.split(',')]
+        # Process temporary string by List Comprehension
+        tt = [x.strip() for x in keys.split(',')]
 
-		keywords.append(tt)
+        keywords.append(tt)
 
-		passes = passes + 1
+        passes = passes + 1
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
+
 def get_index_from_json(data, key, value):
-	' Returns an index reading JSON data to find the value at the particular key '
+    ' Returns an index reading JSON data to find the value at the particular key '
 
-	# Searching key-value pair in all indices
-	for idx in xrange( len(data['insights']['data']) ):
-		if data['insights']['data'][idx][key] == value:
-			return idx
-	
+    # Searching key-value pair in all indices
+    for idx in xrange(len(data['insights']['data'])):
+        if data['insights']['data'][idx][key] == value:
+            return idx
+
+
 def get_int_month(month_str):
-	' Returns from 1-12 the month number from a three letter coded string '	
+    ' Returns from 1-12 the month number from a three letter coded string '
 
-	months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+              "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-	for i in range(len(months)):
-		if month_str == months[i]:
-			return i+1
+    for i in range(len(months)):
+        if month_str == months[i]:
+            return i + 1
+
 
 def process_pub_date(pub_date):
-	' Returns a list containing year month day hour mins extracted from a string of published date'
+    ' Returns a list containing year month day hour mins extracted from a string of published date'
 
-	ymdhm = []
-	ymdhm.append(int(pub_date[8:12]))
-	ymdhm.append(get_int_month(pub_date[0:3]))
-	ymdhm.append(int(pub_date[4:6]))
-	ymdhm.append(int(pub_date[16:18]))
-	ymdhm.append(int(pub_date[19:21]))
+    ymdhm = []
+    ymdhm.append(int(pub_date[8:12]))
+    ymdhm.append(get_int_month(pub_date[0:3]))
+    ymdhm.append(int(pub_date[4:6]))
+    ymdhm.append(int(pub_date[16:18]))
+    ymdhm.append(int(pub_date[19:21]))
 
-	return ymdhm
+    return ymdhm
 
 
 def write_into_csv(data, pdata, owriter, owriter1):
-	' Writes into two csv files data.csv and keywords_data.csv parsing the JSON data file'
+    ' Writes into two csv files data.csv and keywords_data.csv parsing the JSON data file'
 
-        owriter.writerow(['id','name','message','category', 'author', 'likes', 'shares', 'comments', 'ctr', 'year', 'month', 'day','no_of_images', 'head_len', 'no_of_abusive_words'])
-	owriter1.writerow(['id', 'keywords', 'likes', 'shares', 'comments', 'ctr'])
+    owriter.writerow(['id', 'name', 'message', 'category', 'author', 'likes', 'shares', 'comments',
+                      'ctr', 'year', 'month', 'day', 'no_of_images', 'head_len', 'no_of_abusive_words'])
+    owriter1.writerow(['id', 'keywords', 'likes', 'shares', 'comments', 'ctr'])
 
-	# Key and Search Pattern to search for the index
-	key = 'name'
-	search_pattern = "post_stories_by_action_type"
+    # Key and Search Pattern to search for the index
+    key = 'name'
+    search_pattern = "post_stories_by_action_type"
 
-        category_idx=0
-	row = 0
+    category_idx = 0
+    row = 0
 
-	f1 = open('abusive.txt')
-        abusive_words = f1.read()
-        abusive_words = abusive_words.split('\n')
-        f1.close()
+    f1 = open('abusive.txt')
+    abusive_words = f1.read()
+    abusive_words = abusive_words.split('\n')
+    f1.close()
 
-	# Fill data.csv rows for each post
-        for i in range(len(data)):
-		ctr = calc_ctr(data[i])
-	
-		# Use this index value to get likes, comments and shares of each post
-	        idx = get_index_from_json(data[i], key, search_pattern)
+    # Fill data.csv rows for each post
+    for i in range(len(data)):
+        ctr = calc_ctr(data[i])
 
-		if 'name' in data[i].keys():
-                        name = data[i]['name']
-			name = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
-                else:
-                        name = ''
+        # Use this index value to get likes, comments and shares of each post
+        idx = get_index_from_json(data[i], key, search_pattern)
 
-		if 'message' in data[i].keys():
-			message = data[i]['message']
-			message = unicodedata.normalize('NFKD', message).encode('ascii','ignore')
-		else:
-			message = ''
+        if 'name' in data[i].keys():
+            name = data[i]['name']
+            name = unicodedata.normalize(
+                'NFKD', name).encode('ascii', 'ignore')
+        else:
+            name = ''
 
-		if 'like' in data[i]['insights']['data'][idx]['values'][0]['value'].keys():
-                        like = data[i]['insights']['data'][idx]['values'][0]['value']['like']
-                else:
-                        like = 0
+        if 'message' in data[i].keys():
+            message = data[i]['message']
+            message = unicodedata.normalize(
+                'NFKD', message).encode('ascii', 'ignore')
+        else:
+            message = ''
 
+        if 'like' in data[i]['insights']['data'][idx]['values'][0]['value'].keys():
+            like = data[i]['insights']['data'][
+                idx]['values'][0]['value']['like']
+        else:
+            like = 0
 
-		if 'share' in data[i]['insights']['data'][idx]['values'][0]['value'].keys():
-                        share = data[i]['insights']['data'][idx]['values'][0]['value']['share']
-                else:
-                        share = 0
+        if 'share' in data[i]['insights']['data'][idx]['values'][0]['value'].keys():
+            share = data[i]['insights']['data'][
+                idx]['values'][0]['value']['share']
+        else:
+            share = 0
 
-		if 'comment' in data[i]['insights']['data'][idx]['values'][0]['value'].keys():
-                        comment = data[i]['insights']['data'][idx]['values'][0]['value']['comment']
-                else:
-                        comment = 0
+        if 'comment' in data[i]['insights']['data'][idx]['values'][0]['value'].keys():
+            comment = data[i]['insights']['data'][
+                idx]['values'][0]['value']['comment']
+        else:
+            comment = 0
 
-		if pdata[i]['status']=="1":	
-			ymdhm = process_pub_date(pdata[i]['data']['pub_date'])
-			year = ymdhm[0]
-			month = ymdhm[1]
-			day = ymdhm[2]
-			hour = ymdhm[3]
-			mins = ymdhm[4]
-			author = pdata[i]['userData'][0]['display_name']
+        if pdata[i]['status'] == "1":
+            ymdhm = process_pub_date(pdata[i]['data']['pub_date'])
+            year = ymdhm[0]
+            month = ymdhm[1]
+            day = ymdhm[2]
+            hour = ymdhm[3]
+            mins = ymdhm[4]
+            author = pdata[i]['userData'][0]['display_name']
 
-			source = pdata[i]["data"]['article_content']
+            source = pdata[i]["data"]['article_content']
 
-	                soup = BeautifulSoup(source, 'html.parser')
+            soup = BeautifulSoup(source, 'html.parser')
 
-			content = soup
-			
-			if soup.table:
-				soup.table.decompose()
+            content = soup
 
-        	        images = soup.find_all('img')
+            if soup.table:
+                soup.table.decompose()
 
-                	no_of_images = len(images)
+            images = soup.find_all('img')
 
-			videos = soup.find_all('iframe')
+            no_of_images = len(images)
 
-			no_of_videos = len(videos)
+            videos = soup.find_all('iframe')
 
-			head_len = len(pdata[i]['data']['title'])
-			
-			ab_words = []
-			
-			con = soup.get_text()
-                        con = con.split()
+            no_of_videos = len(videos)
 
-                        for word in con :
-                                if word in abusive_words:
-                                        ab_words.append(word)
+            head_len = len(pdata[i]['data']['title'])
 
-                        no_of_abusive_words = len(ab_words)
+            ab_words = []
 
+            con = soup.get_text()
+            con = con.split()
 
-		else:
-			year = 0
-			month = 0
-			day = 0
-			hour = 0
-			mins = 0
-			author = 'Unknown'
-			head_len = 0
-			no_of_images = 0
-			no_of_videos = 0
-			no_of_abusive_words = 0
+            for word in con:
+                if word in abusive_words:
+                    ab_words.append(word)
 
-		owriter.writerow([
-				data[i]['id'],
-				name,
-				message,
-				categories[category_idx],
-				author,
-				like,
-				share, 
-				comment, 
-				ctr,
-				year,
-				month,
-				day,
-				hour,
-				mins,
-				no_of_images,
-				no_of_videos,
-				head_len,
-				no_of_abusive_words])
-		
-		for column in xrange(len(keywords[row])):
-                        owriter1.writerow([
-                                        data[i]['id'],
-                                        keywords[row][column],
-					like,
-                                	share,   
-                                	comment, 
-                                	ctr])
+            no_of_abusive_words = len(ab_words)
 
-		row = row + 1
-                category_idx = category_idx + 1
+        else:
+            year = 0
+            month = 0
+            day = 0
+            hour = 0
+            mins = 0
+            author = 'Unknown'
+            head_len = 0
+            no_of_images = 0
+            no_of_videos = 0
+            no_of_abusive_words = 0
+
+        owriter.writerow([
+            data[i]['id'],
+            name,
+            message,
+            categories[category_idx],
+            author,
+            like,
+            share,
+            comment,
+            ctr,
+            year,
+            month,
+            day,
+            hour,
+            mins,
+            no_of_images,
+            no_of_videos,
+            head_len,
+            no_of_abusive_words])
+
+        for column in xrange(len(keywords[row])):
+            owriter1.writerow([
+                data[i]['id'],
+                keywords[row][column],
+                like,
+                share,
+                comment,
+                ctr])
+
+        row = row + 1
+        category_idx = category_idx + 1
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
+
 
 def calc_ctr(d):
-	' Returns Click-Through Rate i.e. clicks per post reach rate '
+    ' Returns Click-Through Rate i.e. clicks per post reach rate '
 
-	ctr = 0
+    ctr = 0
 
-	# Getting index to know the count of post reaches
-	idx = get_index_from_json(d, 'name', 'post_impressions_unique')
-	
-	if d['insights']['data'][idx]['values'][0]['value']:
-		reach = d['insights']['data'][idx]['values'][0]['value']
-	else:
-		reach=0
+    # Getting index to know the count of post reaches
+    idx = get_index_from_json(d, 'name', 'post_impressions_unique')
 
-	sum_clicks = 0
+    if d['insights']['data'][idx]['values'][0]['value']:
+        reach = d['insights']['data'][idx]['values'][0]['value']
+    else:
+        reach = 0
 
-	# Getting index to know the count of different type of clicks
-	idx = get_index_from_json(d, 'name', 'post_consumptions_by_type')
+    sum_clicks = 0
 
-	for clicks in d['insights']['data'][idx]['values'][0]['value']:
-		sum_clicks = float(d['insights']['data'][idx]['values'][0]['value'][clicks]) + sum_clicks
+    # Getting index to know the count of different type of clicks
+    idx = get_index_from_json(d, 'name', 'post_consumptions_by_type')
 
+    for clicks in d['insights']['data'][idx]['values'][0]['value']:
+        sum_clicks = float(d['insights']['data'][idx]['values'][
+                           0]['value'][clicks]) + sum_clicks
 
-	if sum_clicks==0 and reach==0:
-		ctr =0
-	else:    
-		ctr = format((sum_clicks/reach)*100, '.2f')
+    if sum_clicks == 0 and reach == 0:
+        ctr = 0
+    else:
+        ctr = format((sum_clicks / reach) * 100, '.2f')
 
-	return ctr
+    return ctr
 
 #---------------------------------------------------------------------------------------------------------------------------------------------#
 
+
 def process_link(tmp):
-	' Returns the stripped content from the link of the post '
+    ' Returns the stripped content from the link of the post '
 
-	tmp = tmp[26:]
+    tmp = tmp[26:]
 
-	lc = -1
-	for i in range(len(tmp)):
-		if i!=len(tmp)-1:
-			if tmp[i]=='/' and tmp[i+1]=='?':
-				lc = i
-				break
+    lc = -1
+    for i in range(len(tmp)):
+        if i != len(tmp) - 1:
+            if tmp[i] == '/' and tmp[i + 1] == '?':
+                lc = i
+                break
 
-		elif tmp[i]=='/':
-			lc = i
+        elif tmp[i] == '/':
+            lc = i
 
-	if lc==-1:
-		lc = len(tmp)
+    if lc == -1:
+        lc = len(tmp)
 
-	tmp = tmp[:lc]
+    tmp = tmp[:lc]
 
-	return tmp
+    return tmp
 
-if __name__== '__main__':
-	while True:
-	
-		logging.basicConfig(filename='api.log', level=logging.DEBUG)
- 
-     		logging.debug('loading JSON data. . .\n')
- 
- 		try:
-	                '''r= requests.get('http://10.2.1.35:8087/')
- 
-        	        data = r.json()'''		
+# TODO  Make 2 requests to links and get 2 json and merge and convert it to csv
 
-			with open('data.json') as f:
-				data = json.load(f)
+if __name__ == '__main__':
+    while True:
 
-			post_data = []
-			for d in data:
-				tmp = d['link']
-				if 'http://www.scoopwhoop.com' not in tmp:
-		               		post_data.append({"status":"0"})
-					continue
-			
-				link = process_link(tmp)
-				post_r = requests.get('http://www.scoopwhoop.com/api/v1/'+link)
-				post_data.append(post_r.json())
+        logging.basicConfig(filename='api.log', level=logging.DEBUG)
 
-			csv_file = open('fb_posts_data.csv','w')
-			csv_file1 = open('keywords_data.csv', 'w')
+        logging.debug('loading JSON data. . .\n')
 
-			owriter = csv.writer(csv_file)
-			owriter1 = csv.writer(csv_file1)
+        try:
+            '''r= requests.get('http://10.2.1.35:8087/')
 
-			logging.debug('get page links. . .\n')
+            data = r.json()'''
 
-			pages = get_page_links(data)
+            with open('old_data.json') as f:
+                old_dt = json.load(f)
 
-			logging.debug('loading categories and keywords. . .\n')
+            with open('data.json') as f:
+                data = json.load(f)
 
-			get_data_from_post(pages)
+            for d in old_dt:
+                data.append(d)
 
-			logging.debug('writing into csv files. . .\n')
+            post_data = []
+            for d in data:
+                tmp = d['link']
+                if 'http://www.scoopwhoop.com' not in tmp:
+                    post_data.append({"status": "0"})
+                    continue
 
-			write_into_csv(data, post_data, owriter, owriter1)
+                link = process_link(tmp)
+                post_r = requests.get(
+                    'http://www.scoopwhoop.com/api/v1/' + link)
+                post_data.append(post_r.json())
 
-			logging.debug('data loaded successfully!\n')
+            csv_file = open('fb_posts_data.csv', 'w')
+            csv_file1 = open('keywords_data.csv', 'w')
 
-			csv_file.close()
-			csv_file1.close()
+            owriter = csv.writer(csv_file)
+            owriter1 = csv.writer(csv_file1)
 
-			os.system('python prep_data.py')
+            logging.debug('get page links. . .\n')
 
-		except Exception as e:
-			logging.error(str(e))
+            pages = get_page_links(data)
 
-		time.sleep(7200)
+            logging.debug('loading categories and keywords. . .\n')
+
+            get_data_from_post(pages)
+
+            logging.debug('writing into csv files. . .\n')
+
+            write_into_csv(data, post_data, owriter, owriter1)
+
+            logging.debug('data loaded successfully!\n')
+
+            csv_file.close()
+            csv_file1.close()
+
+            os.system('python prep_data.py')
+
+        except Exception as e:
+            logging.error(str(e))
+
+        time.sleep(7200)
--- prep_data.py
+++ prep_data.py
@@ -14,26 +14,28 @@ import logging
 
 #---------------------------------------------------------------------------------------------------------------------------------------#
 
-def load_data(file_name):
-	' Returns a list from csv data '
-	csv_data = []
-	with open(file_name, 'rb') as f:
-		data = csv.reader(f)
-		for d in data:
-			csv_data.append(d)
 
-	return csv_data
+def load_data(file_name):
+    ' Returns a list from csv data '
+    csv_data = []
+    with open(file_name, 'rb') as f:
+        data = csv.reader(f)
+        for d in data:
+            csv_data.append(d)
+
+    return csv_data
 
 #---------------------------------------------------------------------------------------------------------------------------------------#
-	
+
 Base = declarative_base()
 
+
 class FB_Data(Base):
-    ' Model of data for names and categories '    
+    ' Model of data for names and categories '
 
     __tablename__ = 'FB_POSTS_DATA'
-    
-    id = Column(String, primary_key=True, nullable=False) 
+
+    id = Column(String, primary_key=True, nullable=False)
     name = Column(String(100))
     message = Column(String(100))
     category = Column(String(100))
@@ -55,6 +57,7 @@ class FB_Data(Base):
     head_len = Column(Integer)
     no_of_abusive_words = Column(Integer)
 
+
 class Key_Data(Base):
 
     ' Model of data for keywords'
@@ -73,7 +76,7 @@ class Key_Data(Base):
 if __name__ == "__main__":
     t = time()
 
-    #creating a log file
+    # creating a log file
     logging.basicConfig(filename='api.log', level=logging.DEBUG)
 
     engine = create_engine('sqlite:///myData.sqlite')
@@ -87,79 +90,93 @@ if __name__ == "__main__":
 
     ids = []
 
-    # Prefetch existing IDs
-    for dt in s.query(FB_Data):
-	ids.append(str(dt.id))
+    print dir(FB_Data.__table__)
 
     file_name1 = "fb_posts_data.csv"
     file_name2 = "keywords_data.csv"
-    
+
     fb_data = load_data(file_name1)
     key_data = load_data(file_name2)
 
-    # Adding only those data in the original file which are not already present.
-    for i in range(len(fb_data)):
-	if i>0:
-		if fb_data[i][0] not in ids:
-			ids.append(fb_data[i][0])
-		else:
-			# Pick the existing ID and delete it.
-			dat1 = s.query(FB_Data).filter_by(id=fb_data[i][0]).first()
-			s.delete(dat1)
+    s.close()
+    print "1"
+    sys.exit()
 
-		# Add/update data
-		fb_record = FB_Data(**{
-				'id' : fb_data[i][0],
-				'name' : fb_data[i][1],
-				'message' : fb_data[i][2],
-				'category' : fb_data[i][3],
-				'author' : fb_data[i][4],
-				'like' : fb_data[i][5],
-				'share' : fb_data[i][6],
-				'comment' : fb_data[i][7],
-				'ctr': fb_data[i][8],
-				'year': fb_data[i][9],
-				'month': fb_data[i][10],
-				'day': fb_data[i][11],
-				'hour': fb_data[i][12],
-				'mins': fb_data[i][13],
-				'no_of_images': fb_data[i][14],
-				'no_of_videos': fb_data[i][15],
-				'head_len': fb_data[i][16],
-				'no_of_abusive_words' : fb_data[i][17]
-                         })
-		s.add(fb_record)
+    # Prefetch existing IDs
+    for dt in s.query(FB_Data):
+        ids.append(str(dt.id))
+
+    file_name1 = "fb_posts_data.csv"
+    file_name2 = "keywords_data.csv"
+
+    fb_data = load_data(file_name1)
+    key_data = load_data(file_name2)
+
+    # Adding only those data in the original file which are not already
+    # present.
+    for i in range(len(fb_data)):
+        if i > 0:
+            if fb_data[i][0] not in ids:
+                ids.append(fb_data[i][0])
+            else:
+                # Pick the existing ID and delete it.
+                dat1 = s.query(FB_Data).filter_by(id=fb_data[i][0]).first()
+                s.delete(dat1)
+
+            # Add/update data
+            fb_record = FB_Data(**{
+                'id': fb_data[i][0],
+                'name': fb_data[i][1],
+                'message': fb_data[i][2],
+                'category': fb_data[i][3],
+                'author': fb_data[i][4],
+                'like': fb_data[i][5],
+                'share': fb_data[i][6],
+                'comment': fb_data[i][7],
+                'ctr': fb_data[i][8],
+                'year': fb_data[i][9],
+                'month': fb_data[i][10],
+                'day': fb_data[i][11],
+                'hour': fb_data[i][12],
+                'mins': fb_data[i][13],
+                'no_of_images': fb_data[i][14],
+                'no_of_videos': fb_data[i][15],
+                'head_len': fb_data[i][16],
+                'no_of_abusive_words': fb_data[i][17]
+            })
+            s.add(fb_record)
 
     keys = []
 
     for dt in s.query(Key_Data):
         keys.append(str(dt.keywords))
 
-    # Adding only those data in the original file which are not already present.
+    # Adding only those data in the original file which are not already
+    # present.
     for i in range(len(key_data)):
-	if i>0:
-		if key_data[i][1] not in keys:
-			keys.append(key_data[i][1])
-		else:
-			# Pick the existing ID and delete it.
-			dat2 = s.query(Key_Data).filter_by(keywords=key_data[i][1]).first()
-			s.delete(dat2)
+        if i > 0:
+            if key_data[i][1] not in keys:
+                keys.append(key_data[i][1])
+            else:
+                # Pick the existing ID and delete it.
+                dat2 = s.query(Key_Data).filter_by(
+                    keywords=key_data[i][1]).first()
+                s.delete(dat2)
 
-		# Add/update data
-		key_record = Key_Data(**{
-                                'id' : key_data[i][0],
-				'keywords': key_data[i][1],
-                                'like' : key_data[i][2],
-                                'share' : key_data[i][3],
-                                'comment' : key_data[i][4],
-                                'ctr': key_data[i][5]
-                         })
+            # Add/update data
+            key_record = Key_Data(**{
+                'id': key_data[i][0],
+                'keywords': key_data[i][1],
+                'like': key_data[i][2],
+                'share': key_data[i][3],
+                'comment': key_data[i][4],
+                'ctr': key_data[i][5]
+            })
 
-		s.add(key_record)
+            s.add(key_record)
 
-    s.commit() #Attempt to commit all the records
-   
-    s.close() #Close the connection
-    
+    s.commit()  # Attempt to commit all the records
+
+    s.close()  # Close the connection
+
     logging.debug("Time elapsed: " + str(time() - t) + " s.")
-
